1ea16e553ef8711489b548921070511b3e694294
ruan65 <ruan65@gmail.com>
2018-01-17 20:08:29 +0300

    add form

--- src/components/text_input/TextInput.js
+++ src/components/text_input/TextInput.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 
 class TextInput extends Component {
+
+  state = {
+    title: '',
+    text: ''
+  }
+
+  handleTextInput = (event) => {
+
+    this.setState( {
+
+      ...this.state,
+      text: event.target.value
+
+    } )
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    alert( "Submitted text: " + this.state.text )
+  }
+
   render() {
+
+    const textInputForm =
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input type='text' value={this.state.text} onChange={this.handleTextInput}/>
+        </label>
+
+        <input type='submit' value='Submit'/>
+      </form>
+
     return (
       <div>
-        <p>Hello</p>
+        {textInputForm}
       </div>
     )
   }
